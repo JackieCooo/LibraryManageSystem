5b3aa389a86adb95bc6e9ac7947583044573eddb
JackieCooo <1131390120@qq.com>
2021-10-24 23:29:00 +0800

    beautify main window

--- src/gui/user/window/BookDisplay.java
+++ src/gui/user/window/BookDisplay.java
@@ -7,6 +7,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -18,6 +19,10 @@ import java.io.IOException;
 class BookDisplay extends JButton {
 
     private BufferedImage img;
+    private BufferedImage hoverCover;
+    private boolean isBtn1Set = false;
+    private boolean isBtn2Set = false;
+    private boolean isBtn3Hover = false;
     private final int WIDTH = 150;
     private final int HEIGHT = 200;
 
@@ -37,7 +42,7 @@ class BookDisplay extends JButton {
      * @param radius 圆角半径
      * @return 新的图片对象
      */
-    private BufferedImage setRadius(BufferedImage srcImage, int radius){
+    private BufferedImage setRadius(BufferedImage srcImage, final int radius){
 
 //                if (srcImage.getWidth(null) > width || srcImage.getHeight(null) > height)
 //                {
@@ -61,6 +66,59 @@ class BookDisplay extends JButton {
     }
 
     /**
+     * 将图片转成对应的透明度
+     * @param src 源图片
+     * @param alpha 透明度值
+     * @return 返回目标图片对象
+     */
+    private BufferedImage getAlphaImg(BufferedImage src, final int alpha){
+        BufferedImage dst = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                int rgb = src.getRGB(i, j);
+                if (rgb == 0) continue;
+                Color color = new Color(rgb);
+                color = new Color(color.getRed(), color.getGreen(), color.getBlue(), alpha);
+                dst.setRGB(i, j, color.getRGB());
+            }
+        }
+        return dst;
+    }
+
+    /**
+     * 创建鼠标悬停的样式
+     */
+    private void createHoverIcons(){
+        try {
+            BufferedImage collectBtn;
+            BufferedImage borrowBtn;
+            BufferedImage infoBtn;
+
+            if (!isBtn1Set) collectBtn = ImageIO.read(new File("icons/Uncollected_50px.png"));
+            else collectBtn = ImageIO.read(new File("icons/Collected_50px.png"));
+
+            if (!isBtn2Set) borrowBtn = ImageIO.read(new File("icons/Unborrowed_50px.png"));
+            else borrowBtn = ImageIO.read(new File("icons/Borrowed_50px.png"));
+
+            if (!isBtn3Hover) infoBtn = ImageIO.read(new File("icons/InfoNormal_50px.png"));
+            else infoBtn = ImageIO.read(new File("icons/InfoFocus_50px.png"));
+
+            hoverCover = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g2d = hoverCover.createGraphics();
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+            g2d.setColor(LayoutColors.LIGHT_GRAY);
+            g2d.fillRoundRect(0, 0, WIDTH, HEIGHT, 15, 15);
+            g2d.drawImage(collectBtn, 50, 10, null);
+            g2d.drawImage(borrowBtn, 50, 75, null);
+            g2d.drawImage(infoBtn, 50, 140, null);
+            hoverCover = getAlphaImg(hoverCover, 205);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
      * 初始化书本缩略图
      * @param path 图片路径
      */
@@ -70,6 +128,7 @@ class BookDisplay extends JButton {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        createHoverIcons();
     }
 
     /**
@@ -78,20 +137,36 @@ class BookDisplay extends JButton {
     private void setupListener(){
         this.addMouseListener(new MouseAdapter() {
 
+            /**
+             * 鼠标点击执行收藏操作
+             * @param e 鼠标事件对象
+             */
             @Override
             public void mouseClicked(MouseEvent e) {
-
+                Point p = e.getPoint();
             }
 
+        });
+
+        this.addMouseMotionListener(new MouseMotionAdapter() {
+
+            /**
+             * 鼠标移动到图标时改变指针样式
+             * @param e 鼠标事件对象
+             */
             @Override
-            public void mouseEntered(MouseEvent e) {
-                setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+            public void mouseMoved(MouseEvent e) {
+                Point p = e.getPoint();
+                if (p.x >= 50 && p.x <= 100 && ((p.y >= 10 && p.y <= 60) || (p.y >= 75 && p.y <= 125) || (p.y >= 140 && p.y <= 190))) {
+                    setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+                }
+                else {
+                    setCursor(Cursor.getDefaultCursor());
+                }
+                isBtn3Hover = p.x >= 50 && p.x <= 100 && p.y >= 140 && p.y <= 190;
+                repaint();
             }
 
-            @Override
-            public void mouseExited(MouseEvent e) {
-                setCursor(Cursor.getDefaultCursor());
-            }
         });
     }
 
@@ -103,7 +178,12 @@ class BookDisplay extends JButton {
 
         UIDefaults btnDefaults = new UIDefaults();
         btnDefaults.put("Button.backgroundPainter", (Painter<JComponent>)(g2d, c, w, h) -> {
-            g2d.setColor(Color.BLUE);
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+            img = setRadius(img, 15);
+            g2d.drawImage(img, 0, 0, null);
+        });
+        btnDefaults.put("Button[MouseOver].backgroundPainter", (Painter<JComponent>)(g2d, c, w, h) -> {
             g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
             g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
             img = setRadius(img, 15);
--- src/test/BookDisplayTest.java
+++ src/test/BookDisplayTest.java
@@ -7,6 +7,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -20,6 +21,7 @@ class BookDisplay extends JButton {
     private BufferedImage img;
     private BufferedImage unselectedIcon;
     private BufferedImage selectedIcon;
+    private boolean isBtnSet = false;
     private final int WIDTH = 150;
     private final int HEIGHT = 200;
 
@@ -62,24 +64,53 @@ class BookDisplay extends JButton {
         return image;
     }
 
-//    private BufferedImage getAlphaImg(BufferedImage src, final int alpha){
-//
-//    }
+    /**
+     * 将图片转成对应的透明度
+     * @param src 源图片
+     * @param alpha 透明度值
+     * @return 返回目标图片对象
+     */
+    private BufferedImage getAlphaImg(BufferedImage src, final int alpha){
+        BufferedImage dst = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                int rgb = src.getRGB(i, j);
+                if (rgb == 0) continue;
+                Color color = new Color(rgb);
+                color = new Color(color.getRed(), color.getGreen(), color.getBlue(), alpha);
+                dst.setRGB(i, j, color.getRGB());
+            }
+        }
+        return dst;
+    }
 
-    private void createHoverIcons(String unselectedIconPath, String selectedIconPath){
+    /**
+     * 创建鼠标悬停的样式
+     * @param unselectedIconPath 未收藏时的按钮图标路径
+     * @param selectedIconPath 收藏时的按钮图标路径
+     */
+    private void createHoverIcons(final String unselectedIconPath, final String selectedIconPath){
         try {
             BufferedImage unselectedImg = ImageIO.read(new File(unselectedIconPath));
             unselectedIcon = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
             Graphics2D g2d = unselectedIcon.createGraphics();
-            g2d.setColor(LayoutColors.BLUE);
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+            g2d.setColor(LayoutColors.LIGHT_GRAY);
             g2d.fillRoundRect(0, 0, WIDTH, HEIGHT, 15, 15);
-            g2d.drawImage(unselectedImg, 0, 0, null);
+            g2d.drawImage(unselectedImg, 45, 70, null);
+            unselectedIcon = getAlphaImg(unselectedIcon, 205);
 
             BufferedImage selectedImg = ImageIO.read(new File(selectedIconPath));
+            selectedIcon = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
             g2d = selectedIcon.createGraphics();
-            g2d.setColor(LayoutColors.BLUE);
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+            g2d.setColor(LayoutColors.LIGHT_GRAY);
             g2d.fillRoundRect(0, 0, WIDTH, HEIGHT, 15, 15);
-            g2d.drawImage(unselectedImg, 0, 0, null);
+            g2d.drawImage(selectedImg, 45, 70, null);
+            g2d.dispose();
+            selectedIcon = getAlphaImg(selectedIcon, 205);
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -96,7 +127,7 @@ class BookDisplay extends JButton {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        createHoverIcons("icons/Uncollected.png", "icons/Collected.png");
+        createHoverIcons("icons/Uncollected_60px.png", "icons/Collected_60px.png");
     }
 
     /**
@@ -105,19 +136,35 @@ class BookDisplay extends JButton {
     private void setupListener(){
         this.addMouseListener(new MouseAdapter() {
 
+            /**
+             * 鼠标点击执行收藏操作
+             * @param e 鼠标事件对象
+             */
             @Override
             public void mouseClicked(MouseEvent e) {
+                isBtnSet = !isBtnSet;
             }
 
+        });
+
+        this.addMouseMotionListener(new MouseMotionAdapter() {
+
+            /**
+             * 鼠标移动到收藏图标时改变指针样式
+             * @param e 鼠标事件对象
+             */
             @Override
-            public void mouseEntered(MouseEvent e) {
-                setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+            public void mouseMoved(MouseEvent e) {
+                Point p = e.getPoint();
+                if (p.x >= 45 && p.x <= 105 && p.y >= 70 && p.y <= 130) {
+                    setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+                }
+                else {
+                    setCursor(Cursor.getDefaultCursor());
+                }
+                repaint();
             }
 
-            @Override
-            public void mouseExited(MouseEvent e) {
-                setCursor(Cursor.getDefaultCursor());
-            }
         });
     }
 
@@ -139,7 +186,8 @@ class BookDisplay extends JButton {
             g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
             img = setRadius(img, 15);
             g2d.drawImage(img, 0, 0, null);
-            g2d.drawImage(unselectedIcon, 0, 0, null);
+            if (isBtnSet) g2d.drawImage(selectedIcon, 0, 0, null);
+            else g2d.drawImage(unselectedIcon, 0, 0, null);
         });
         this.putClientProperty("Nimbus.Overrides", btnDefaults);
         this.putClientProperty("Nimbus.Overrides.InheritDefaults", false);
