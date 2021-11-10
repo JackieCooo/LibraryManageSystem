22e62991602ee9a98ff845fa18dd9756e547a4ac
JackieCooo <1131390120@qq.com>
2021-11-11 00:23:41 +0800

    test chart

--- src/test/ChartTest.java
+++ src/test/ChartTest.java
@@ -1,5 +1,15 @@
 package test;
 
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.CategoryAxis;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.LineAndShapeRenderer;
+import org.jfree.data.category.DefaultCategoryDataset;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.plaf.basic.BasicLabelUI;
@@ -9,6 +19,8 @@ import java.io.File;
 import java.io.IOException;
 
 public class ChartTest {
+
+
 
     public static void main(String[] args) {
 
@@ -22,11 +34,93 @@ public class ChartTest {
             }
         }
 
+        class Chart extends ChartPanel {
+
+            private static DefaultCategoryDataset dataset;
+            private static JFreeChart chart;
+
+            static {
+                setupData();
+                setupChart();
+            }
+
+            public Chart(Object[][] data) {
+                super(chart);
+                this.setupUI();
+                this.setData(data);
+            }
+
+            public void setData(Object[][] data){
+                for(Object[] i : data){
+                    dataset.addValue((Integer)i[0], (String)i[1], (String)i[2]);
+                }
+            }
+
+            private void setupUI() {
+                this.setPreferredSize(new Dimension(730, 200));
+            }
+
+            private static void setupData() {
+                dataset = new DefaultCategoryDataset();
+/*
+                dataset.addValue(140, "", "1月");
+                dataset.addValue(57, "", "2月");
+                dataset.addValue(95, "", "3月");
+                dataset.addValue(333, "", "4月");
+                dataset.addValue(293, "", "5月");
+                dataset.addValue(285, "", "6月");
+                dataset.addValue(519, "", "7月");
+                dataset.addValue(109, "", "8月");
+                dataset.addValue(167, "", "9月");
+                dataset.addValue(576, "", "10月");
+                dataset.addValue(363, "", "11月");
+                dataset.addValue(551, "", "12月");
+*/
+            }
+
+            private static void setupChart() {
+                CategoryAxis categoryAxis = new CategoryAxis();
+                categoryAxis.setTickLabelFont(new Font("微软雅黑", Font.PLAIN, 14));
+
+                ValueAxis valueAxis = new NumberAxis();
+                valueAxis.setTickLabelFont(new Font("微软雅黑", Font.PLAIN, 14));
+
+                LineAndShapeRenderer renderer = new LineAndShapeRenderer(true, false);
+                renderer.setSeriesPaint(0, Color.BLUE);
+                renderer.setSeriesStroke(0, new BasicStroke(5, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+
+                CategoryPlot plot = new CategoryPlot(dataset, categoryAxis, valueAxis, renderer);
+                plot.setBackgroundPaint(Color.WHITE);
+                plot.setOutlineVisible(false);
+                plot.setDomainGridlinesVisible(false);
+                plot.setRangeGridlinesVisible(false);
+                plot.setOrientation(PlotOrientation.VERTICAL);
+
+                chart = new JFreeChart("访问量", new Font("黑体", Font.BOLD, 20), plot, false);
+                chart.setBackgroundPaint(Color.WHITE);
+            }
+        }
+
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setBounds(100, 100, 400, 300);
-
+        frame.setBounds(100, 100, 800, 600);
+        Object[][] data = {
+            {140, "", "1月"},
+            {57, "", "2月"},
+            {95, "", "3月"},
+            {333, "", "4月"},
+            {293, "", "5月"},
+            {285, "", "6月"},
+            {519, "", "7月"},
+            {109, "", "8月"},
+            {167, "", "9月"},
+            {576, "", "10月"},
+            {363, "", "11月"},
+            {551, "", "12月"}
+        };
+        Chart c = new Chart(data);
         JPanel panel = new JPanel();
+        panel.add(c);
         frame.add(panel);
         frame.setVisible(true);
     }
